c072d3ee86599e609667c650e4f0263033c80ec6
quittheshit <miklhagstroem@gmail.com>
2025-12-08 23:52:03 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -250,7 +250,7 @@ function App() {
 
             {/* Subtext */}
             <p className="fade-in stagger-4 text-white/90 px-6 mb-10 max-w-3xl mx-auto" style={{fontSize: '1rem', lineHeight: '1.6'}}>
-              Bliv byens GO-TO firma i din branche med en top-position i søgeresultaterne, byens bedste hjemmeside og automatiseret kundeservice mm.
+              Bliv byens GO-TO firma i din branche med en top-position i søgeresultaterne, byens bedste hjemmeside, professionel markedsføring og automatiseret kundeservice mm.
             </p>
 
             {/* CTA Button */}
