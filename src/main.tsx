304b15020ef14b06f57ab98adc126b226cb83306
quittheshit <miklhagstroem@gmail.com>
2026-03-31 21:52:39 +0200

    installing Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import * as Sentry from '@sentry/react';
 import App from './App.tsx';
 import './index.css';
+
+Sentry.init({
+  dsn: "https://aed606809173440f2af1a4a915a85591@o4511140791451648.ingest.de.sentry.io/4511140809736272",
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions (reduce in production)
+  // Session Replay (Optional but recommended)
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
